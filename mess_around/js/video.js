7ed7bde2b2b1435941ebef5c8f3b9151c4362ddf
cool_cool <zwlddiao@gmail.com>
2022-04-19 22:06:03 +0800

    add all file

---
+++ mess_around/js/video.js
@@ -0,0 +1,19 @@
+
+function move1() {
+    document.getElementById("button2").onclick == function () {
+        document.getElementById("button1").style.display = "block";
+        document.getElementById("button2").style.display = "none";
+        ved.play();
+    }
+};
+
+function move2() {
+    document.getElementById("button1").onclick = function () {
+        document.getElementById("button1").style.display = "none";
+        document.getElementById("button3").style.display = "block";
+        document.getElementById("kepu").style.display = "block";
+    }
+};
+
+
+
